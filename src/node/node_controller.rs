d2cd8248bfd78ad7a88f3267c242920bcfbb9197
linxx5599 <linxx5599@163.com>
2024-11-15 11:37:03 +0800

    feat: created update delete  node

--- src/node/node_controller.rs
+++ src/node/node_controller.rs
@@ -1,5 +1,9 @@
-use rocket::{get, post, routes, Route};
+use k8s_openapi::api::core::v1::Node;
+use rocket::serde::json::Json;
+use rocket::{delete, get, post, put, routes, Route};
 use serde_json::{json, Value};
+
+use crate::lib::utils;
 
 #[path = "./node_service.rs"]
 mod node_service;
@@ -10,12 +14,47 @@ async fn get_node() -> Value {
     json!(result)
 }
 
-#[post("/node")]
-fn create_node() -> Value {
-    json!("Hello, node!")
+#[post("/node", data = "<node_body>")]
+async fn create_node(node_body: Json<Node>) -> Value {
+    match Some(node_body) {
+        Some(params) => {
+            let result = node_service::create_node(params).await;
+            json!(result)
+        }
+        None => {
+            json!({"code": 400, "message": "参数异常"})
+        }
+    }
+}
+
+#[put("/node?<name>", data = "<node_body>")]
+async fn update_node(name: Option<&str>, node_body: Json<Node>) -> Value {
+    let name = utils::validate_and_set_str::<i32>(name);
+    if name.is_empty() {
+        return json!({"code": 400, "message": "name is empty"});
+    }
+    match Some(node_body) {
+        Some(params) => {
+            let result = node_service::update_node(&name, params).await;
+            json!(result)
+        }
+        None => {
+            json!({"code": 400, "message": "参数异常"})
+        }
+    }
+}
+
+#[delete("/node?<name>")]
+async fn delete_node(name: Option<&str>) -> Value {
+    let name = utils::validate_and_set_str::<i32>(name);
+    if name.is_empty() {
+        return json!({"code": 400, "message": "name is empty"});
+    }
+    let result = node_service::delete_node(&name).await;
+    json!(result)
 }
 
 // 定义一个函数来返回所有路由
 pub fn routes() -> Vec<Route> {
-    routes![get_node, create_node]
+    routes![get_node, create_node, update_node, delete_node]
 }
--- src/node/node_service.rs
+++ src/node/node_service.rs
@@ -1,5 +1,7 @@
 use k8s_openapi::api::core::v1::Node;
+use kube::api::{DeleteParams, Patch, PatchParams, PostParams};
 use kube::{api::ObjectList, Api};
+use rocket::serde::json::Json;
 use serde_json::{json, to_value, Value};
 
 use crate::common_mod::get_root_error;
@@ -20,6 +22,60 @@ pub async fn get_node() -> Value {
             let mut msg = String::from("504: Gateway Timeout");
             msg.push_str(&get_root_error(&err).to_string());
             json!(&msg)
+        }
+    }
+}
+
+pub async fn create_node(pod_body: Json<Node>) -> Value {
+    let client = kube_client::MKubeClient::new().await.unwrap();
+    let nodes: Api<Node> = Api::all(client);
+    let data = pod_body.into_inner();
+    let params = PostParams::default();
+    match nodes.create(&params, &data).await {
+        Ok(pod) => json!(&pod),
+        Err(err) => {
+            json!({
+                "code": 400,
+                "message": get_root_error(&err).to_string(),
+            })
+        }
+    }
+}
+
+pub async fn update_node(name: &str, pod_body: Json<Node>) -> Value {
+    let client = kube_client::MKubeClient::new().await.unwrap();
+    let nodes: Api<Node> = Api::all(client);
+    let patch = pod_body.into_inner();
+    let params = PatchParams::default();
+    let patch = Patch::Apply(&patch);
+    match nodes.patch(name, &params, &patch).await {
+        Ok(pod) => json!(&pod),
+        Err(err) => {
+            json!({
+                "code": 400,
+                "message": get_root_error(&err).to_string(),
+            })
+        }
+    }
+}
+
+pub async fn delete_node(name: &str) -> Value {
+    let client = kube_client::MKubeClient::new().await.unwrap();
+    let nodes: Api<Node> = Api::all(client);
+    let params = DeleteParams::default();
+    match nodes.delete(name, &params).await {
+        Ok(resp) => match &resp.left() {
+            Some(pod) => json!(pod),
+            None => json!({
+                "code": 400,
+                "message": format!("nodes {} not found: NotFound", name),
+            }),
+        },
+        Err(err) => {
+            json!({
+                "code": 400,
+                "message": get_root_error(&err).to_string(),
+            })
         }
     }
 }
